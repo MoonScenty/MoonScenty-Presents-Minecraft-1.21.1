a2248c215fe8d32b95c15eb09bcea64b15b2c275
MoonScenty <silvia_s@naver.com>
2026-08-16 01:20:13 +0900

    광물 부스러기 제련 레시피 추가
    Create: Metallurgy의 용해로에서 부스러기를 녹여 용융 금속을
    생산하는 레시피를 추가합니다.
    
    - 열 요구치는 Create Low-Heated의 lowheated 단계
    - 처리 시간 2초, 산출량 45mb

--- kubejs/server_scripts/materials.js
+++ kubejs/server_scripts/materials.js
@@ -8,16 +8,26 @@ ServerEvents.recipes(event => {
   }).id('kubejs:crafting/mortar')
 
   const filings = [
-    ['kubejs:iron_filings', 'minecraft:raw_iron', 'iron'],
-    ['kubejs:copper_filings', 'minecraft:raw_copper', 'copper'],
-    ['kubejs:gold_filings', 'minecraft:raw_gold', 'gold'],
-    ['kubejs:zinc_filings', 'create:raw_zinc', 'zinc'],
-    ['kubejs:wolframite_filings', 'createmetallurgy:raw_tungsten', 'wolframite']
+    ['kubejs:iron_filings', 'minecraft:raw_iron', 'iron', 'createmetallurgy:molten_iron'],
+    ['kubejs:copper_filings', 'minecraft:raw_copper', 'copper', 'createmetallurgy:molten_copper'],
+    ['kubejs:gold_filings', 'minecraft:raw_gold', 'gold', 'createmetallurgy:molten_gold'],
+    ['kubejs:zinc_filings', 'create:raw_zinc', 'zinc', 'createmetallurgy:molten_zinc'],
+    ['kubejs:wolframite_filings', 'createmetallurgy:raw_tungsten', 'wolframite', 'createmetallurgy:molten_tungsten']
   ]
 
   filings.forEach(([output, rawOre, metal]) => {
     event.shapeless(output, [rawOre, 'kubejs:mortar'])
       .damageIngredient('kubejs:mortar')
       .id(`kubejs:crafting/${metal}_filings`)
+  })
+
+  filings.forEach(([filing, , metal, molten]) => {
+    event.custom({
+      type: 'createmetallurgy:melting',
+      heat_requirement: 'lowheated',
+      ingredients: [{ item: filing }],
+      processing_time: 40,
+      results: [{ amount: 45, id: molten }]
+    }).id(`kubejs:melting/${metal}_filings`)
   })
 })
